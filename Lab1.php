32558fc28cabec904cea840be266025c878d8812
Tafsir-69 <mdtafsirrahman77@gmail.com>
2025-11-02 14:35:59 +0600

    Describe your changes

--- Lab1.php
+++ Lab1.php
@@ -1,11 +1,65 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Clinic Patient Registration</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Hi Anime</h1>
+    <h2>Clinic Patient Registration</h2>
+    <table>
+        <tr>
+            <td>Full Name:</td>
+            <br>
+            <td><input type="text" placeholder="Enter your full name" required></td>
+        </tr>
+        <tr>
+            <td>Age:</td>
+            <br>
+            <td><input type="number" placeholder="Enter your age" required></td>
+        </tr>
+      
+        <tr>
+            <td>Email:</td>
+            
+            <td><input type="email" placeholder="Enter email address"></td>
+        </tr>
+       
+        <tr>
+            <td>Insurance Provider:</td>
+            <td>
+                <select>
+                    <option value="">--Select--</option>
+                    
+                </select>
+            </td>
+        </tr>
+       <tr>
+        <td>Insurance Policy Number:</td>
+        <td><input type="text"></td>
+       </tr>
+       <tr>
+    <td colspan="2" style="text-align:center;">
+        <h4>Additional Information</h4>
+       
+    </td>
+</tr>
+ <tr>
+            <td>Username:</td>
+            <td><input type="text" placeholder="Choose a username" required></td>
+        </tr>
+        <tr>
+            <td>Password:</td>
+            <td><input type="password" placeholder="Enter password" required></td>
+        </tr>
+        <tr>
+            <td>Confirm Password:</td>
+            <td><input type="password" placeholder="Confirm password" required></td>
+        </tr>
+        <tr>
+    <td colspan="2" style="text-align:center;">
+        <button type="button">Register</button>
+    </td>
+</tr>
+    </table>
 </body>
 </html>
